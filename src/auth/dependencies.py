c852fd6414ec4ae0d75ea669fe13399e03ff4d81
Francisco Madera <franciscomaderanatera@gmail.com>
2025-12-07 23:06:49 -0400

    feat: endpoint de perfil protegido

---
+++ src/auth/dependencies.py
@@ -0,0 +1,38 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from sqlalchemy.orm import Session
+from src.user.models import User
+from config.database import get_db
+from config.security import SECRET_KEY, ALGORITHM
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = payload.get("user_id")
+
+        if user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token inválido",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token inválido o expirado",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+    return user
--- src/user/routes.py
+++ src/user/routes.py
@@ -3,11 +3,16 @@ from sqlalchemy.orm import Session
 from config.database import get_db
 from src.user.models import User
 from src.user.schema import UserRead
+from src.auth.dependencies import get_current_user
 
 router = APIRouter()
 
 
-@router.get("/", response_model=list[UserRead])
-def get_users(db: Session = Depends(get_db)):
-    users = db.query(User).all()
-    return users
+@router.get("/")
+def get_profile(current_user: User = Depends(get_current_user)):
+    return {
+        "id": current_user.id,
+        "name": current_user.name,
+        "username": current_user.username,
+        "created_at": current_user.created_at,
+    }
